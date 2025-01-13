adc1c13562355f7bbd606dd27a205fb20742d9bd
Vishesh-Dwivedi-Git <visheshdwivedi225544@gmail.com>
2025-01-13 23:52:31 +0530

    WebSockets

--- week18/Nextjs/my-app/app/User/page.tsx
+++ week18/Nextjs/my-app/app/User/page.tsx
@@ -3,9 +3,6 @@ import axios from "axios"
 import { useEffect,useState } from "react";
 
 export default async function User(){
-  new Promise=setTimeout(() => {
-    
-  }, );
   const response = await axios.get("");
   const data=response.data;
 
