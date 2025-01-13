cad23b41d13e9f15e33f16d0f776d9cb2bef207c
Vishesh-Dwivedi-Git <visheshdwivedi225544@gmail.com>
2025-01-14 01:39:36 +0530

    websocket server

--- week16/WEB_SOCKETS/dist/index.js
+++ week16/WEB_SOCKETS/dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("hi");
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+//websocket server 
+const wss = new ws_1.WebSocketServer({ port: 8081 });
+wss.on("connection", function (socket) {
+    socket.on("message", (e) => {
+        const message = e.toString();
+        console.log("Received:", message);
+        if (message.trim() === "ping") { // Ensure trimming whitespace
+            socket.send("pong");
+        }
+        else {
+            socket.send("Unknown message");
+        }
+    });
+});
--- week16/WEB_SOCKETS/src/index.ts
+++ week16/WEB_SOCKETS/src/index.ts
@@ -1,2 +1,18 @@
-console.log("hi");
- 
+import { WebSocketServer } from "ws";
+//websocket server 
+
+const wss=new WebSocketServer({port:8081});
+
+wss.on("connection",function(socket){   //like app.get
+    socket.on("message", (e) => {
+        const message = e.toString();
+        console.log("Received:", message);
+      
+        if (message.trim() === "ping") { // Ensure trimming whitespace
+          socket.send("pong");
+        } else {
+          socket.send("Unknown message");
+        }
+      });
+      
+});
