9de57c0823de2931e038a0b186c57b469765d543
Vishesh Dwivedi <visheshdwivedi225544@gmail.com>
2025-05-24 23:59:35 +0530

    feat:turborepo

--- week21/chatApp/apps/web/app/chat/(roomId)/page.tsx
+++ week21/chatApp/apps/web/app/chat/(roomId)/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import TextInput from "@repo/ui/text-input";
+
 export default function ChatRoom(){
       return (
-         <div>
-            Chat Room
+         <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            height: '100vh',
+            fontSize: '24px',
+            color: '#333',
+            
+         }}>
+            <div>
+               Chat Room 
+            </div>
+
+            <div> 
+               
+                      <TextInput placeholder="Enter the Room" onChange={()=>{
+                        alert(
+                          "Message is Sent"
+                        );
+                      }}  ></TextInput>
+            </div>
             
          </div>
       )
